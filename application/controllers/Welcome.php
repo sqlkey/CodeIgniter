72cd6f03dbea2e905d308f14f27f690405cf205d
sqlkey <47800307+sqlkey@users.noreply.github.com>
2019-02-22 06:23:09 +0200

    Add files via upload

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,4 +1,4 @@
-<?php
+    <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
@@ -20,6 +20,9 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
+		$this->load->view('includes/head');
 		$this->load->view('welcome_message');
+		$this->load->view('index');
+
 	}
 }
